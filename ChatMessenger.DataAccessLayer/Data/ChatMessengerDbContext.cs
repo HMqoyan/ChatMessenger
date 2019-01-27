70831b161fb3c1286e4a7e834d4981153b754f71
Henrik Mqoyan <hen_mqoyan@outlook.com>
2019-01-28 00:09:47 +0400

    add db context

---
+++ ChatMessenger.DataAccessLayer/Data/ChatMessengerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatMessenger.DataAccessLayer.Data
+{
+    public class ChatMessengerDbContext : DbContext
+    {
+        public ChatMessengerDbContext(DbContextOptions<ChatMessengerDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
