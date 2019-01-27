c6db471908c3e873938c4b4c9b287dfe70e7fd68
Henrik Mqoyan <hen_mqoyan@outlook.com>
2019-01-28 01:11:36 +0400

    Add generic repository

---
+++ ChatMessenger.Core/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChatMessenger.Core.Interfaces.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatMessenger.Core.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity
+    {
+        Task<ICollection<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(int entityId);
+        Task<TEntity> Create(TEntity entity);
+        Task Delete(int entityId);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ ChatMessenger.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using ChatMessenger.Core.Interfaces.Models.Db;
+using ChatMessenger.Core.Interfaces.Repositories;
+using ChatMessenger.DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatMessenger.DataAccessLayer.Repositories
+{
+    internal abstract class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity
+        : class, IEntity
+    {
+        protected readonly ChatMessengerDbContext _dbContext;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(ChatMessengerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public virtual async Task<ICollection<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetAsync(int entityId)
+        {
+            return await _dbSet.FindAsync(entityId);
+        }
+
+        public virtual async Task<TEntity> Create(TEntity entity)
+        {
+            return (await _dbSet.AddAsync(entity)).Entity;
+        }
+
+        public virtual async Task Delete(int entityId)
+        {
+            TEntity entityToDelete = await GetAsync(entityId);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            if (_dbContext.Entry(entity).State == EntityState.Deleted)
+            {
+                _dbSet.Attach(entity);
+            }
+
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
