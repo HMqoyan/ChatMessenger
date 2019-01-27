b2bb2e5c9e0b248556b82eed0b0a9c2792de4c14
Henrik Mqoyan <hen_mqoyan@outlook.com>
2019-01-28 01:24:56 +0400

    add repositories

---
+++ ChatMessenger.DataAccessLayer/Repositories/MessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChatMessenger.Core.Interfaces.Repositories;
+using ChatMessenger.Core.Models.Db;
+using ChatMessenger.DataAccessLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatMessenger.DataAccessLayer.Repositories
+{
+    internal class MessageRepository : GenericRepository<Message>, IMessageRepository
+    {
+        public MessageRepository(ChatMessengerDbContext dbContext) 
+            : base(dbContext)
+        {
+        }
+    }
+}
---
+++ ChatMessenger.DataAccessLayer/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChatMessenger.Core.Interfaces.Repositories;
+using ChatMessenger.Core.Models.Db;
+using ChatMessenger.DataAccessLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatMessenger.DataAccessLayer.Repositories
+{
+    internal class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(ChatMessengerDbContext dbContext) 
+            : base(dbContext)
+        {
+
+        }
+    }
+}
